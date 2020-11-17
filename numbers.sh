c6557836c55031eea973e131706527c0b327d93d
Connor Lydon <connoralydon@gmail.com>
2020-11-17 11:54:13 -0800

    bash script to tell if set of numbers are odd/even

---
+++ numbers.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+# numbers.sh
+# Connor Lydon
+
+echo "Type a positive integer: "
+read -r N
+echo ""
+
+for i in $(seq 1 $N)
+
+do
+  mod=$(( $i % 2 ))
+
+  if [ $mod -eq 0 ]
+  then
+    echo "$i Even"
+  else
+    echo "$i Odd"
+  fi
+
+done
+echo ""
+echo "Thank You"
